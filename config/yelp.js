8868b896d23749cd60daf167f93163581fad6802
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-04 11:42:54 -0800

    Added Yelp's API

---
+++ config/yelp.js
@@ -0,0 +1,38 @@
+var Yelp = require('yelp');
+
+var yelp = new Yelp({
+  consumer_key: process.env.CONSUMER_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET,
+  token: process.env.TOKEN,
+  token_secret: process.env.TOKEN_SECRET
+});
+
+
+//------------- How to use Yelp's documentation -------------//
+
+
+// See http://www.yelp.com/developers/documentation/v2/search_api
+// yelp.search({ term: 'food', location: 'Montreal' })
+// .then(function (data) {
+//   console.log(data);
+// })
+// .catch(function (err) {
+//   console.error(err);
+// });
+
+// See http://www.yelp.com/developers/documentation/v2/business
+// yelp.business('yelp-san-francisco')
+//   .then(console.log)
+//   .catch(console.error);
+
+// yelp.phoneSearch({ phone: '+15555555555' })
+//   .then(console.log)
+//   .catch(console.error);
+
+// A callback based API is also available:
+// yelp.business('yelp-san-francisco', function(err, data) {
+//   if (err) return console.log(error);
+//   console.log(data);
+// });
+
+module.exports = yelp;
--- server.js
+++ server.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+// added these for Yelp's API
+var oauthSignature = require('oauth-signature');
+var n = require('nonce')();
+var request = require('request');
+require('dotenv').config();
 require('./config/database');
 
 var index = require('./routes/index');
