0f09cf1912aa18da8d05e867ba76b3107ad4ab7d
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-06 11:32:25 -0800

    working on user controller

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var userCtrl = require('../controllers/users');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/', userCtrl.index);
+router.get('/new', userCtrl.new);
+router.post('/', userCtrl.create);
+router.get('/:id/edit', userCtrl.edit);
+router.put('/:id', userCtrl.update);
+router.delete('/:id', userCtrl.remove);
 
 module.exports = router;
