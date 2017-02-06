1233971ec92cf4c7b5062a20f46e6940f9ed27b8
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-05 23:35:24 -0800

    display JSON

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,9 @@
 var express = require('express');
 var router = express.Router();
-var yelp = require('../config/yelp');
-const rootURL = 'https://api.yelp.com/';
-
+var yelpCtrl = require('../controllers/yelp');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  yelp.search({term: 'euro cafe', location: 'Irvine, CA', price: '1,2,3', limit: 1})
-  .then(function (data) {
-    console.log(data);
-    res.json(data);
-  })
-  .catch(function (err) {
-      console.error(err);
-  });
-});
+router.get('/', yelpCtrl.userDetails);
+router.get('/search', yelpCtrl.search);
 
 module.exports = router;
