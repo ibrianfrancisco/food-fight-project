c6324b6ab36706d4b4694a9438728ead436da5ec
Holly <hnguyen0711@gmail.com>
2017-02-06 15:44:25 -0800

    added random background image

---
+++ public/javascripts/main.js
@@ -0,0 +1,6 @@
+//var totalCount = 5;//
+
+//function ChangeIt() {
+//	var num = Math.ceil( Math.random() * totalCount );
+//	document.getElementById('main-wrapper').style.background = 'public/assets/images/'+ num +'.jpg';
+//}
