5b8e761903d52f5bf235d6b7145bd260016d76ae
Brian Francisco <ibrianfrancisco@gmail.com>
2017-02-06 17:08:05 -0800

    search bars work

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,7 @@ var yelpCtrl = require('../controllers/api/yelp');
 // CARLIE START
 var passport = require('passport');
 var yelp = require('../config/yelp');
-const rootURL = 'https://api.yelp.com/';
-
+// const rootURL = 'https://api.yelp.com/';
 
 
 /* GET home page. */
@@ -14,9 +13,30 @@ router.get('/', function(req, res, next) {
     res.render('index', { user: req.user });
 });
 
+// Yelp API routes
 router.get('/', yelpCtrl.userDetails);
-router.get('/search', yelpCtrl.search);
+// router.post('/search', yelpCtrl.search);
+router.post('/', function(req, res, next) {
+  var term = req.body.term;
+  // console.log(term);
+  yelp.search({ term: term, location: req.body.location })
+  .then(function (data) {
+    var jsonBussObj = JSON.parse(data); // Parse JSON string to JSON Object
+    // console.log(data);
+    // console.log(jsonBussObj);
+    res.render('index', {jsonBussObj});
+  })
+  .catch(function (err) {
 
+    console.error('error', err);
+  });
+})
+
+
+
+
+
+// OAuth routes
 router.get('/auth/google', passport.authenticate(
   'google',
   { scope: ['profile', 'email'] }
