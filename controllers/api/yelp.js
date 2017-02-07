4d6401ac695c4b6d99a6b56b494db00b8186ab5d
chris baird <cab3953@gmail.com>
2017-02-07 15:24:13 -0800

    idk

--- controllers/api/yelp.js
+++ controllers/api/yelp.js
@@ -1,24 +1,17 @@
 var request = require('request');
 var yelp = require('../../config/yelp');
-
 module.exports = {
   search: search
 }
-
 function search(req, res) {
   var term = req.body.term;
   yelp.search({ term: term, location: req.body.location, limit: 50 })
   .then(function (data) {
     var jsonBussObj = JSON.parse(data); // Parse JSON string to JSON Object
     console.log(jsonBussObj);
-    res.render('show', {jsonBussObj});
+    res.render('show', {jsonBussObj, user: req.user});
   })
   .catch(function (err) {
     console.error('error', err);
   });
 }
-
-function userDetails(req, res) {
-  res.render('index', {title: 'Express'});
-}
-
