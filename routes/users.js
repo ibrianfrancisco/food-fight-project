82c12efd79275f64994699af5d5ac1cb294c68bb
Holly <hnguyen0711@gmail.com>
2017-02-08 13:54:11 -0800

    added partials, seperated css

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/user', function(req, res, next) {
+  res.render('user', { user: req.user });
+});
+
 module.exports = router;
