36b116e6ab5caafd8ca08f5df0342cd234c92d3d
Holly <hnguyen0711@gmail.com>
2017-02-09 23:49:32 -0800

    fixed heroku crash

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userCtrl = require('../controllers/users');
 
-=======
 /* GET users listing. */
 // router.get('/', function(req, res, next) {
 //   res.send('respond with a resource');
@@ -17,7 +16,7 @@ router.get('/user', function(req, res, next) {
 
 router.get('/', function(req, res, next) {
   res.render('user', { user: req.user });
-})
+});
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) return next();
