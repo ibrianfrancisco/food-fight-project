4e9c117bfbd13e894d071770861e4efdeede1451
Carlie Clippinger <carliesachiko@gmail.com>
2017-02-09 20:49:21 -0800

    nearly done

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,7 @@ var express = require('express');
 var router = express.Router();
 var userCtrl = require('../controllers/users');
 
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
 router.get('/', isLoggedIn, userCtrl.show);
-
 
 router.get('/user', function(req, res, next) {
   res.render('user', { user: req.user });
@@ -17,12 +11,6 @@ router.get('/user', function(req, res, next) {
 router.get('/', function(req, res, next) {
   res.render('user', { user: req.user });
 })
-
-// router.get('/userShow', usersCtrl.show);
-// router.get('/user/:id', function(req, res, next) {
-//   res.render('userShow', {user: req.user})
-// });
-// router.post('/favorite', usersCtrl.favorite);
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) return next();
